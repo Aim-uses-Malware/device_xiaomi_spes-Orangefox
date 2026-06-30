475e6f0839fd75e326f7fdf336c64b2d7e4ef833
aimrite2 <137486127+Aim-uses-Malware@users.noreply.github.com>
2026-06-30 22:30:59 +0500

    Update FOX build type and maintainer in vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -49,9 +49,8 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
         export OF_NO_MIUI_PATCH_WARNING=1
 	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
 	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	export FOX_VERSION="R11.1"
-        export FOX_BUILD_TYPE="Beta"
-	export OF_MAINTAINER="JashkiratV"
+        export FOX_BUILD_TYPE="Unofficial"
+	export OF_MAINTAINER="aimrite2"
 	export OF_NO_SPLASH_CHANGE=1
         export FOX_VIRTUAL_AB_DEVICE=1
         export OF_ADVANCED_SECURITY=1
