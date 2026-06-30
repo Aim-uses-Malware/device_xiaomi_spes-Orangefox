0237a628ea97cda9cdb1c305a90b525ee31c46c6
aimrite2 <137486127+Aim-uses-Malware@users.noreply.github.com>
2026-06-30 22:29:50 +0500

    Change build type to Unofficial and update maintainer

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -55,8 +55,8 @@ if [ "$1" = "$FDEVICE" ] || [ "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     export OF_NO_MIUI_PATCH_WARNING=1
     export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
     export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-    export FOX_BUILD_TYPE="Stable"
-    export OF_MAINTAINER="JashkiratV"
+    export FOX_BUILD_TYPE="Unofficial"
+    export OF_MAINTAINER="aimrite2"
     export FOX_VIRTUAL_AB_DEVICE=1
     export OF_ADVANCED_SECURITY=1
 
